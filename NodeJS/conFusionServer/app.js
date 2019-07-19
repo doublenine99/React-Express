1c970eeb8254a09265d613f286dfd9f0bdb67a87
jiujiu <pjj851135136@gmail.com>
2019-07-19 21:52:06 +0800

    mongoose population

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,8 +12,9 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 const Dishes = require('./models/dishes');
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 var session = require('express-session');
@@ -34,42 +36,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser('12345-67890-09876-54321'));
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
 
 app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.session());
+
 // enable the rootpage and userRouter to signup or login
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// authenticating function
-function auth(req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    next(err);
-  }
-  else {
-    next();
-  }
-}
-// start using authentication to prevent unexpect users
-app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
-
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
